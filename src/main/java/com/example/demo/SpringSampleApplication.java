40f7a6bf96782a719899ce12835f9d3fad1e8d7e
chiharukakimoto <qianchun@hotmail.co.jp>
2021-04-05 21:46:42 +0900

    add Hello World

--- src/main/java/com/example/demo/SpringSampleApplication.java
+++ src/main/java/com/example/demo/SpringSampleApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSampleApplication {
-	
-    String home() {
-        return "Hello World!";
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSampleApplication.class, args);
