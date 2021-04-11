01c7b4a8a7648db6072b0d8b8aeeb392c6433270
chiharukakimoto <qianchun@hotmail.co.jp>
2021-04-12 07:35:36 +0900

    3-3 3-4

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.trySpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+
+	@Autowired
+	private HelloService helloService;
+	private Integer integer;
 
 	@GetMapping("/hello")
 	public String getHello() {
@@ -19,5 +24,15 @@ public class HelloController {
 		model.addAttribute("sample", str);
 		return "helloResponse";
 	}
+
+@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2")String str, Model model){
+		int id = integer.parseInt(str);
+		Employee employee = helloService.findOne(id);
+		model.addAttribute("id", employee.getEmployeeId());
+		model.addAttribute("name", employee.getEmployeeName());
+		model.addAttribute("age", employee.getAge());
+		return "helloResponseDB";
+}
 
 }
